8652394a84252993bc7ed185da798521b0f29f48
Shreyo <mastershreyo15082002@gmail.com>
2023-09-10 19:18:53 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function clearAll() {
+    document.getElementById('display').value = '';
+}
+
+function clearLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
